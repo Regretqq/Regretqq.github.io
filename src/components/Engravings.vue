8ca2978f95cab3f1f186cc96686adad051e5c380
felix <felix.uimonen@gmail.com>
2022-04-08 18:51:28 +0300

    adding dist subtree

---
+++ src/components/Engravings.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div>
+    <b-button variant="danger">asd</b-button>
+    <b-dropdown v-model="SealName1" text="engraving" class="m-md-2">
+      <b-dropdown-item>{{}}</b-dropdown-item>
+
+    </b-dropdown>
+    <input v-model="sealCount1" type="number">
+  </div>
+</template>
+
+<script>
+import { BButton } from 'bootstrap-vue'
+import Vue from "vue";
+Vue.component('b-button', BButton)
+export default {
+
+  name: "Engravings",
+
+
+  data() {
+    return {
+      combatEng: ["Grudge", "Cursed Doll", "Hit master"],
+      classEng: ["Peacekeeper", "Time to hunt"],
+      sealCount1: 0,
+      SealName1: ''
+    }
+  },
+  computed:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
